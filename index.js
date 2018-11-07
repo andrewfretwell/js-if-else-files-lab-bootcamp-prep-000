8ada8362e46810ac235b2022e024646245596da9
Andrew Fretwell <andrewfretwell@gmail.com>
2018-11-07 21:40:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var name = "Joe"
 var height = "74"
+console.log("Name:")
+console.log(name)
+
+console.log("Height:")
+console.log(height)
 
 // Don't worry about this
 module.exports = { name, height
